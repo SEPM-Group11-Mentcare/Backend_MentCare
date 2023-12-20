d8104e227996c972f7515f5de85ee620793ce4ba
xian-94 <s3957034@rmit.edu.vn>
2023-12-20 19:12:48 +0700

    apply pagination for viewing therapists

--- controllers/therapistController.js
+++ controllers/therapistController.js
@@ -1,12 +1,20 @@
 const Therapist = require('../models/therapist');
 const ErrorHandler = require('../utils/errorHandler');
 
-// Get all therapists
+// Get all therapists with pagination
 module.exports.getTherapists = async (req, res, next) => {
     try {
-        const therapists = await Therapist.find();
+        // Get page number and number of items per page
+        const page = parseInt(req.query.page) || 1;
+        const itemsPerPage = parseInt(req.query.size) || 10;
+        // Calculate skip values
+        const skip = (page-1) * itemsPerPage;
+
+        const therapists = await Therapist.find().skip(skip).limit(itemsPerPage);
+        const total = (await Therapist.find()).length
+
         // Response
-        res.status(200).json(therapists);
+        res.status(200).json({therapists: therapists, total: total});
     }
 
     catch (err) {
