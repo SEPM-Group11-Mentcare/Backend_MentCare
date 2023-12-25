298f0b2e5aa3f98a5ade080b6e30b02d5a371ef0
HaAnh18 <102302068+HaAnh18@users.noreply.github.com>
2023-12-25 12:37:15 +0700

    done update schedule for therapists

--- controllers/therapistController.js
+++ controllers/therapistController.js
@@ -2,6 +2,7 @@ const Admin = require("../models/admin");
 const Therapist = require("../models/therapist");
 const ErrorHandler = require("../utils/errorHandler");
 const Patient = require("../models/patient");
+const Schedule = require("../models/schedule");
 
 exports.setSchedule = async (req, res, next) => {
   const data = {
@@ -22,7 +23,31 @@ exports.setSchedule = async (req, res, next) => {
   //   return day;
   // });
 
-  data.schedules.map((date) => {
-    await 
+  data.schedules.map(async(date) => {
+    await Schedule.create({dateTime: date, therapist: data.user})
+    .catch((err) => {
+      next(new ErrorHandler(err.message, 404));
+    })
   })
+
+  res.status(200).json("Create successfully")
 };
+
+exports.getSchedule = async(req, res, next) => {
+  const schedules = await Schedule.find({therapist: "656985db94e13306d13304d3"})
+  .catch((err) => {
+    next(new ErrorHandler(err.message, 404));
+  })
+
+  res.status(200).json(schedules);
+}
+
+exports.deleteSchedule = async(req, res, next) => {
+  await Schedule.findByIdAndDelete(req.params.id)
+  .then(() => {
+    res.status(200).json("Delete successfully");
+  })
+  .catch((err) => {
+    next(new ErrorHandler(err.message, 404));
+  })
+}
--- routes/therapist.js
+++ routes/therapist.js
@@ -4,4 +4,8 @@ const therapist = require('../controllers/therapistController')
 
 router.post('/schedule', therapist.setSchedule)
 
+router.get('/schedules', therapist.getSchedule);
+
+router.delete('/delete/:id', therapist.deleteSchedule);
+
 module.exports = router
