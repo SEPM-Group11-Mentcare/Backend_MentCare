e4a223a1370fb14e613068c7e17eeefd956ee8a9
vothanhthong <tthong.vox@gmail.com>
2024-01-21 11:51:28 +0700

    Optimize Code

--- models/journal.js
+++ models/journal.js
@@ -3,14 +3,13 @@ const { format } = require("date-fns");
 
 const journalSchema = new mongoose.Schema({
   journalTitle: { type: String, required: true },
-  journalText: { type: String, required: true },
+  journalText: { type: String },
   createdDate: {
     type: Date,
     default: new Date(),
   },
   mood: { type: String, required: true },
-  patient: { type: mongoose.Schema.Types.ObjectId,
-    ref: 'Patient' },
+  patient: { type: mongoose.Schema.Types.ObjectId, ref: "Patient" },
 });
 
 const Journal = mongoose.model("Journal", journalSchema);
